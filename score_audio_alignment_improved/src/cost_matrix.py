c11d67e5728ccb9e3aaca9e8394de249ae64118f
Timotej Peťko <108617475+trimotoj@users.noreply.github.com>
2026-04-13 18:56:32 +0200

    Delete score_audio_alignment_improved directory

--- score_audio_alignment_improved/src/cost_matrix.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def cosine_cost_matrix(
-    score_chroma: np.ndarray,
-    audio_chroma: np.ndarray,
-) -> np.ndarray:
-    similarity = np.clip(score_chroma @ audio_chroma.T, -1.0, 1.0)
-    return 1.0 - similarity
